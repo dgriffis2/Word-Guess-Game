ebbfbc1e3c385db91db52b28dbda3def137220a5
dgriffis2 <dgriffis2@gmail.com>
2018-06-27 20:50:38 -0700

    added js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,135 @@
 var words = ["khaleesi","tyrion","cersei", "stark", "targaryen", "lannister", "hodor", "winterfell", "tyrell", "daenerys"]
-
+//"khaleesi","tyrion","cersei", "stark", "targaryen", "lannister", "hodor", "winterfell", "tyrell", "daenerys"
     var correct = 0;
     var wrong = 0;
-    var numberGuessRem = 10
-    if (numberGuessRem === 10){
-        var randomWord = words[Math.floor(Math.random()*words.length)];}
+    var wrongList = []
+    var correctList = []
+    var numberGuessRem = 13
+    var randomWord = words[Math.floor(Math.random()*words.length)];
+    var correctLetter = 0;
+    var wrongLetter = 0;
+    var win = 0;
+    var currentWord = ""
 document.onkeyup = function (event) {
+    currentWord = ""
     var userInput = event.key;
     var numLetters =[]
     numLetters.push(randomWord);
     var actualNumberLetters = randomWord.length;
     var letters = randomWord.split("");
     var sub = letters.indexOf(userInput);
-    console.log("sub " + sub);
-console.log("word " + randomWord);
-console.log("Number of letters " + actualNumberLetters);
-console.log("user input " + userInput);
+    var noDup = wrongList.indexOf(userInput)
+    var noDupCorrect = correctList.indexOf(userInput)
+    var noWin = ""
 
+  //  console.log("sub " + sub);
+//console.log("word " + randomWord);
+//console.log("Number of letters " + actualNumberLetters);
+//console.log("user input " + userInput);
 
-
-if(sub === -1 && numberGuessRem !== 0){
-    numberGuessRem--
-
+if(numberGuessRem === 0){
+    numberGuessRem = 13
+    randomWord = words[Math.floor(Math.random()*words.length)];
+    //console.log("New Word " + randomWord)
 }
+
+ 
+//<script>
+//function myFunction() {
+    //var str = document.getElementById("demo").innerHTML; 
+    //var res = str.replace("Microsoft", "W3Schools");
+    //document.getElementById("demo").innerHTML = res;
+//}
+//</script>
+
+
+//var myEventList=[];
+//document.getElementById('myID').onclick = function(event) {
+   //myEventList.push(event)
+//};
+
+
+
+
+
+if(sub === -1 && numberGuessRem !== 0 && noDup === -1){
+    numberGuessRem--
+    wrongList.push(userInput)
+}
+
+
+// on key press generate word[0] loop through all the words
     //if
     //(userInput !== letters[i])
+
+
     
     for (var i = 0; i < letters.length; i++) {
-        if (userInput === letters[i]) {
+        if (userInput === letters[i] && noDupCorrect === -1) {
             correct++
-        //}{
-            //console.log(sub + " SUB")
+            correctList.push(userInput)
         }
     }
+
+
+
+    if(numberGuessRem === 0){
+        wrongList = []
+        correctList = []
+        noWin = "Sorry you lost!"
+        currentWord = randomWord
+    }
+
+
+    //if(array1.sort().join(',')=== array2.sort().join(',')){
+      //  alert('same members');
+    //}
+    //else alert('not a match');
         
-        console.log("correct " + correct)
-        console.log("wrong " + wrong)
-        console.log("Number of Guesses Remaining " + numberGuessRem)
+    var sortCorrect = correctList.sort()
+    var sortLetters = letters.sort()
+
+
+    function winner (arr1, arr2) {
+        if(arr1.length !== arr2.length)
+            return false;
+        for(var i = arr1.length; i--;) {
+            if(arr1[i] !== arr2[i])
+                return false;
+        }
+    
+        return true;
+    }
+    
+
+    if(winner(sortCorrect,sortLetters) === true){
+        wrongList = []
+        correctList = []
+        win++
+        currentWord = randomWord
+        numberGuessRem = 13
+        randomWord = words[Math.floor(Math.random()*words.length)];
+    }
+
+
+
+
+
+    console.log("sortCorrect " + sortCorrect)
+    console.log("sortLetters " + sortLetters)
+    
+    //if(correctList.sort() === letters.sort()){
+        //win++
+    //}
+
+
+
+
+        //console.log("letters " + letters)
+        //console.log("correct list" + correctList)
+        //console.log("correct " + correct)
+        //console.log("wrong " + wrong)
+        //console.log("Number of Guesses Remaining " + numberGuessRem)
     //if (confirmSushi) {
       //  alert("You like " + sushiType + "!");
      // }    
@@ -50,20 +143,29 @@ if(sub === -1 && numberGuessRem !== 0){
 //var res = str.split("");
 
 
+var html =
+"<p>" + win + " " + noWin + " " + currentWord + "</p>"
+
+document.getElementById("wins").innerHTML = html;
+
 
 var html =
-"<p>Who will win? " + words[0] + "</p>"
+"<p>Number of Letters " + letters.length + "</p>"
 
 document.getElementById("current_word").innerHTML = html;
 
+var html =
+"<p>" + correctList + "</p>"
+
+document.getElementById("correct_guess").innerHTML = html;
 
 var html =
-"<p>Who will win? " + words[1] + "</p>"
+"<p>" + numberGuessRem + "</p>"
 
 document.getElementById("rem_guess").innerHTML = html;
 
 var html =
-"<p>Who will win? " + words[9] + "</p>"
+"<p>"+ wrongList + "</p>"
 
 document.getElementById("al_guess").innerHTML = html;
 
